5f0608a5d68fa77c453f67ff29799728eaeafd86
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-08-06 21:13:49 +0200

    Created basic project and Maven structure.

---
+++ tinustris-parent/tinustris-assembly/files/bin/tinustris.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Startup script for Tinustris.
+:: This script assumes that a valid javaw.exe, version 1.7 or later, is on the PATH.
+
+set PWD="%CD%"
+
+set LIB_DIR=%PWD%\..\lib
+
+set CONFIG_DIR=%PWD%\..\etc
+
+set JAVA_OPTS=
+
+set JAVA=javaw
+
+start %JAVA% %JAVA_OPTS% -cp %CONFIG_DIR%;%LIB_DIR%\* nl.mvdr.tinustris.gui.TinusTris
