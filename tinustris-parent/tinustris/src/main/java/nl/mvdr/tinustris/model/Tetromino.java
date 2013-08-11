da9c11622c206f48e07681a8aa0652700c5beae6
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-08-11 19:47:55 +0200

    Added a source directory as well as the Tetromino enum.

---
+++ tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/model/Tetromino.java
@@ -0,0 +1,76 @@
+package nl.mvdr.tinustris.model;
+
+/**
+ * Representation of a Tetromino; that is, one of the seven groups of four blocks that can fall into the basin.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public enum Tetromino {
+    /**
+     * I, or straight shape.
+     * 
+     * <pre>
+     * +
+     * +
+     * +
+     * +
+     * </pre>
+     */
+    I,
+    /**
+     * O, or square shape.
+     * 
+     * <pre>
+     * ++
+     * ++
+     * </pre>
+     */
+    O,
+    /**
+     * T shape.
+     * 
+     * <pre>
+     * +++
+     *  +
+     * </pre>
+     */
+    T,
+    /**
+     * J shape.
+     * 
+     * <pre>
+     *  +
+     *  +
+     * ++
+     * </pre>
+     * */
+    J,
+    /**
+     * L shape.
+     * 
+     * <pre>
+     * +
+     * +
+     * ++
+     * </pre>
+     */
+    L,
+    /**
+     * S skew shape.
+     * 
+     * <pre>
+     *  ++
+     * ++
+     * </pre>
+     */
+    S,
+    /**
+     * Z skew shape.
+     * 
+     * <pre>
+     * ++
+     *  ++
+     * </pre>
+     */
+    Z
+}
