36d41d3173f42e074215a042dfbbd975460b0e73
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-08-11 22:34:00 +0200

    First version of the game state.

---
+++ tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/model/GameState.java
@@ -0,0 +1,172 @@
+package nl.mvdr.tinustris.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * Representation of the game state for a single Tetris player.
+ * 
+ * @author Martijn van de Rijdt
+ */
+// TODO write a better toString implementation, with a nice ascii art representation of the grid
+@ToString
+@EqualsAndHashCode
+@Getter
+public class GameState {
+    /** Minimum width of a Tetris grid. */
+    private static final int MIN_WIDTH = 4;
+    /** Minimum height of a Tetris grid. */
+    private static final int MIN_HEIGHT = 4;
+    /** Default width of a Tetris grid. */
+    private static final int DEFAULT_WIDTH = 10;
+    /** Default height of a Tetris grid. */
+    private static final int DEFAULT_HEIGHT = 22;
+
+    /**
+     * The basin of blocks.
+     * 
+     * Element (i, j) of the grid is represented by grid[i * width + j]. If the element is null, that space in the grid
+     * is empty; otherwise it contains a block. The specific Tetromino value indicates the tetromino that the block was
+     * originally a part of, which may affect how it is represented graphically.
+     * 
+     * The current block (that the payer is still controlling as it falls) is not contained in the grid.
+     * 
+     * Note that the top two rows of the grid should not be visible.
+     * 
+     * The value of this field is not modified; in fact it should preferably be an unmodifiable list.
+     */
+    @NonNull
+    private final List<Tetromino> grid;
+    /** Width of the grid. */
+    private final int width;
+    /** Current block. May be null, if the game is in the process of showing a cutscene (like a disappearing line). */
+    private final Tetromino currentBlock;
+    // TODO location and turn state of the current block
+    /** The next block in line. */
+    @NonNull
+    // TODO replace by a queue of blocks, in case we want to be able to display multiple "next" blocks
+    private final Tetromino nextBlock;
+
+    /** Constructor for a (new) game with a completely empty grid of default size. */
+    public GameState() {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    /**
+     * Constructor for a (new) game with a completely empty grid.
+     * 
+     * @param width
+     *            width of the basin, should be at least 1
+     * @param height
+     *            height of the basin, should be at least 3
+     */
+    public GameState(int width, int height) {
+        super();
+
+        checkWidth(width);
+        checkHeight(height);
+
+        this.width = width;
+        ArrayList<Tetromino> tempGrid = new ArrayList<>(width * height);
+        while (tempGrid.size() != width * height) {
+            tempGrid.add(null);
+        }
+        this.grid = Collections.unmodifiableList(tempGrid);
+        this.currentBlock = null;
+        this.nextBlock = Tetromino.I; // TODO determine randomly?
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param grid
+     *            grid
+     * @param width
+     *            width of the grid
+     * @param currentBlock
+     *            current block
+     * @param nextBlock
+     *            next block
+     */
+    public GameState(@NonNull List<Tetromino> grid, int width, Tetromino currentBlock, @NonNull Tetromino nextBlock) {
+        super();
+
+        checkWidth(width);
+        if (grid.size() % width != 0) {
+            throw new IllegalArgumentException("Unexpected grid size, should be a multiple of width " + width
+                    + ", was: " + grid.size());
+        }
+
+        this.grid = grid;
+        this.width = width;
+
+        checkHeight(getHeight());
+
+        this.currentBlock = currentBlock;
+        this.nextBlock = nextBlock;
+    }
+
+    /**
+     * Checks that the given number is at least the minimum grid width.
+     * 
+     * @param w
+     *            value to be checked
+     * @throws IllegalArgumentException
+     *             in case the value is incorrect
+     */
+    private void checkWidth(int w) {
+        if (w < MIN_WIDTH) {
+            throw new IllegalArgumentException("width should be at least " + MIN_WIDTH + ", was: " + w);
+        }
+    }
+
+    /**
+     * Checks that the given number is at least the minimum grid height.
+     * 
+     * @param height
+     *            value to be checked
+     * @throws IllegalArgumentException
+     *             in case the value is incorrect
+     */
+    private void checkHeight(int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException("height should be at least " + MIN_HEIGHT + ", was: " + height);
+        }
+    }
+
+    /** @return the grid's height */
+    public int getHeight() {
+        return this.grid.size() / this.width;
+    }
+
+    /**
+     * Returns the block in the grid at index i, j. Does not take the player's currently controlled block into account.
+     * 
+     * @param x
+     *            x coordinate
+     * @param y
+     *            y coordinate
+     * @return block at x, y; null if there is no block there
+     */
+    // TODO do take the currently controlled block into account?
+    public Tetromino getBlock(int x, int y) {
+        if (x < 0 || getWidth() <= x) {
+            throw new IndexOutOfBoundsException(String.format(
+                    "x coordinate in (%s, %s) is out of bounds, should be between 0 and %s.",
+                    Integer.valueOf(x), Integer.valueOf(y), Integer.valueOf(width)));
+        }
+        if (y < 0 || getHeight() <= y) {
+            throw new IndexOutOfBoundsException(String.format(
+                    "y coordinate in (%s, %s) is out of bounds, should be between 0 and %s.",
+                    Integer.valueOf(x), Integer.valueOf(y), Integer.valueOf(getHeight())));
+        }
+
+        return this.grid.get(x + y * this.width);
+    }
+}
---
+++ tinustris-parent/tinustris/src/test/java/nl/mvdr/tinustris/model/GameStateTest.java
@@ -0,0 +1,276 @@
+package nl.mvdr.tinustris.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class for {@link GameState}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Slf4j
+public class GameStateTest {
+    /** Test case for the default constructor. */
+    @Test
+    public void testDefaultConstructor() {
+        GameState gameState = new GameState();
+
+        log.info(gameState.toString());
+        Assert.assertEquals(10, gameState.getWidth());
+        Assert.assertEquals(22, gameState.getHeight());
+        for (Tetromino block : gameState.getGrid()) {
+            Assert.assertNull(block);
+        }
+        Assert.assertNotNull(gameState.getNextBlock());
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test
+    public void testWidthHeightConstructor() {
+        GameState gameState = new GameState(38, 12);
+
+        log.info(gameState.toString());
+        Assert.assertEquals(38, gameState.getWidth());
+        Assert.assertEquals(12, gameState.getHeight());
+        for (Tetromino block : gameState.getGrid()) {
+            Assert.assertNull(block);
+        }
+        Assert.assertNotNull(gameState.getNextBlock());
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test
+    public void testWidthHeightConstructorMinimum() {
+        GameState gameState = new GameState(4, 4);
+
+        log.info(gameState.toString());
+        Assert.assertEquals(4, gameState.getWidth());
+        Assert.assertEquals(4, gameState.getHeight());
+        for (Tetromino block : gameState.getGrid()) {
+            Assert.assertNull(block);
+        }
+        Assert.assertNotNull(gameState.getNextBlock());
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorNegativeWidth() {
+        new GameState(-4, 22);
+    }
+    
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorZeroWidth() {
+        new GameState(0, 22);
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorTooSmallWidth() {
+        new GameState(1, 22);
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorNegativeHeight() {
+        new GameState(10, -4);
+    }
+    
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorZeroHeight() {
+        new GameState(10, 0);
+    }
+
+    /** Test case for the constructor with custom width and height values. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWidthHeightConstructorTooSmallHeight() {
+        new GameState(10, 1);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test
+    public void testConstructor() {
+        List<Tetromino> grid = createGrid(64);
+        
+        GameState gameState = new GameState(grid, 8, Tetromino.L, Tetromino.Z);
+        
+        log.info(gameState.toString());
+        Assert.assertEquals(8, gameState.getWidth());
+        Assert.assertEquals(8, gameState.getHeight());
+        Assert.assertEquals(grid, gameState.getGrid());
+        Assert.assertEquals(Tetromino.L, gameState.getCurrentBlock());
+        Assert.assertEquals(Tetromino.Z, gameState.getNextBlock());
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test
+    public void testConstructorMinimalSize() {
+        List<Tetromino> grid = createGrid(16);
+        
+        GameState gameState = new GameState(grid, 4, Tetromino.L, Tetromino.Z);
+        
+        log.info(gameState.toString());
+        Assert.assertEquals(4, gameState.getWidth());
+        Assert.assertEquals(4, gameState.getHeight());
+        Assert.assertEquals(grid, gameState.getGrid());
+        Assert.assertEquals(Tetromino.L, gameState.getCurrentBlock());
+        Assert.assertEquals(Tetromino.Z, gameState.getNextBlock());
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorEmptyGrid() {
+        new GameState(Collections.<Tetromino>emptyList(), 4, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNegativeWidth() {
+        List<Tetromino> grid = createGrid(16);
+        
+        new GameState(grid, -4, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorZeroWidth() {
+        List<Tetromino> grid = createGrid(16);
+        
+        new GameState(grid, 0, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorTooSmallWidth() {
+        List<Tetromino> grid = createGrid(16);
+        
+        new GameState(grid, 2, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorTooSmallHeight() {
+        List<Tetromino> grid = createGrid(8);
+        
+        new GameState(grid, 4, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case for the constructor where all required fields are simply passed in. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNotDivisableByWidth() {
+        List<Tetromino> grid = createGrid(65);
+        
+        new GameState(grid, 8, Tetromino.L, Tetromino.Z);
+    }
+    
+    /** Test case where a game state is copied. */
+    @Test
+    public void testConstructorCopy() {
+        GameState gameState0 = new GameState();
+        
+        GameState gameState1 = new GameState(gameState0.getGrid(), gameState0.getWidth(), gameState0.getCurrentBlock(),
+                gameState0.getNextBlock());
+        
+        Assert.assertEquals(gameState0, gameState1);
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test
+    public void testGetBlockNoBlocksInGrid() {
+        GameState gameState = new GameState();
+        log.info(gameState.toString());
+
+        for(int x = 0; x != 10; x++) {
+            for (int y = 0; y != 22; y++) {
+                Tetromino block = gameState.getBlock(x, y);
+                
+                Assert.assertNull(block);
+            }
+        }
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test
+    public void testGetBlock() {
+        List<Tetromino> grid = new ArrayList<>(10 * 22);
+        while (grid.size() != 10 * 22) {
+            grid.add(Tetromino.O);
+        }
+        grid = Collections.unmodifiableList(grid);
+        GameState gameState = new GameState(grid, 10, Tetromino.L, Tetromino.Z);
+        log.info(gameState.toString());
+
+        for(int x = 0; x != 10; x++) {
+            for (int y = 0; y != 22; y++) {
+                Tetromino block = gameState.getBlock(x, y);
+                
+                Assert.assertEquals(Tetromino.O, block);
+            }
+        }
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBlockXTooSmall() {
+        GameState gameState = new GameState();
+        log.info(gameState.toString());
+
+        gameState.getBlock(-1,  4);
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBlockXTooLarge() {
+        GameState gameState = new GameState();
+        log.info(gameState.toString());
+
+        gameState.getBlock(35,  4);
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBlockYTooSmall() {
+        GameState gameState = new GameState();
+        log.info(gameState.toString());
+
+        gameState.getBlock(4,  -1);
+    }
+    
+    /** Test case for the getBlock method. */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBlockYTooLarge() {
+        GameState gameState = new GameState();
+        log.info(gameState.toString());
+
+        gameState.getBlock(4,  35);
+    }
+    
+    /**
+     * Creates a list of tetrominoes.
+     * 
+     * @param size number of items in the list
+     * @return list
+     */
+    private List<Tetromino> createGrid(int size) {
+        List<Tetromino> values = new ArrayList<>(Tetromino.values().length);
+        values.add(null);
+        values.addAll(Arrays.asList(Tetromino.values()));
+        
+        List<Tetromino> result = new ArrayList<>();
+        int i = 0;
+        while (result.size() != size) {
+            result.add(values.get(i));
+            i = (i + 1) % values.size();
+        }
+        result = Collections.unmodifiableList(result);
+        return result;
+    }
+}
