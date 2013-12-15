cd791c31ab01aa0de6298d29b9115417d8a5366c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-12-15 18:30:52 +0100

    Determining which actions to take.

--- tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/engine/TinusTrisEngine.java
+++ tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/engine/TinusTrisEngine.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import nl.mvdr.tinustris.input.Input;
 import nl.mvdr.tinustris.input.InputState;
 import nl.mvdr.tinustris.input.InputStateHistory;
+import nl.mvdr.tinustris.model.Action;
 import nl.mvdr.tinustris.model.GameState;
 import nl.mvdr.tinustris.model.Orientation;
 import nl.mvdr.tinustris.model.Point;
@@ -20,6 +22,14 @@ public class TinusTrisEngine implements GameEngine {
     /** Number of frames between drops. */
     // TODO have this be variable, depending on current level
     private static final int FRAMES_BETWEEN_DROPS = 60;
+    /**
+     * Number of frames the input is ignored while the user is holding down a button.
+     * 
+     * Say this value is 30 and the user is holding the left button. The active block will now move left once every
+     * thirty frames.
+     */
+    // TODO come up with a better name for this constant?
+    private static final int INPUT_FRAMES = 30;
 
     /** Tetromino generator. */
     private final TetrominoGenerator generator;
@@ -47,6 +57,7 @@ public class TinusTrisEngine implements GameEngine {
     @Override
     public GameState computeNextState(GameState previousState, InputState inputState) {
         // TODO implement for realsies
+        determineActions(previousState, inputState);
 
         List<Tetromino> grid;
         int width = previousState.getWidth();
@@ -88,5 +99,26 @@ public class TinusTrisEngine implements GameEngine {
         
         return new GameState(grid, width, block, location, orientation, nextBlock, numFramesSinceLastTick,
                 inputStateHistory, blockCounter);
+    }
+
+    /**
+     * Determines which actions should be performed.
+     * 
+     * @param previousState previous game state
+     * @param inputState input state
+     * @return actions
+     */
+    private List<Action> determineActions(GameState previousState, InputState inputState) {
+        List<Action> actions = new ArrayList<>(Action.values().length + 1);
+        if (previousState.getNumFramesSinceLastTick() + 1 == FRAMES_BETWEEN_DROPS) {
+            actions.add(Action.MOVE_DOWN);
+        }
+        for (Input input: Input.values()) {
+            if (inputState.isPressed(input)
+                    && previousState.getInputStateHistory().getNumberOfFrames(input) % INPUT_FRAMES == 0) {
+                actions.add(input.getAction());
+            }
+        }
+        return actions;
     }
 }
