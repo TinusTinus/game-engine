3c635207594ffc7aefbea3c1df6f77e7eb64c49e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-01-01 19:57:34 +0100

    Fixed TGM2 curves.

--- tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/engine/speedcurve/TheGrandMaster2MasterSpeedCurve.java
+++ tinustris-parent/tinustris/src/main/java/nl/mvdr/tinustris/engine/speedcurve/TheGrandMaster2MasterSpeedCurve.java
@@ -12,17 +12,22 @@ import nl.mvdr.tinustris.model.GameState;
  */
 public class TheGrandMaster2MasterSpeedCurve implements SpeedCurve {
     /** Internal gravity curve. */
-    private RangedCurve internalGravityCurve;
+    private final RangedCurve internalGravityCurve;
+    /** Lock delay curve/ */
+    private final RangedCurve lockDelayCurve;
+    /** ARE curve. */
+    private final RangedCurve areCurve;
+    /** Line curve. */
+    private final RangedCurve lineClearCurve;
 
     /** Constructor. */
+    @SuppressWarnings("serial") // maps aren't to be serialised
     public TheGrandMaster2MasterSpeedCurve() {
         super();
         
-        @SuppressWarnings("serial") // not to be serialised
         SortedMap<Integer, Integer> map = new TreeMap<Integer, Integer>() {{
             put(Integer.valueOf(0), Integer.valueOf(4));
-            put(Integer.valueOf(8), Integer.valueOf(5));
-            put(Integer.valueOf(19), Integer.valueOf(6));
+            put(Integer.valueOf(30), Integer.valueOf(6));
             put(Integer.valueOf(35), Integer.valueOf(8));
             put(Integer.valueOf(40), Integer.valueOf(10));
             put(Integer.valueOf(50), Integer.valueOf(12));
@@ -30,31 +35,52 @@ public class TheGrandMaster2MasterSpeedCurve implements SpeedCurve {
             put(Integer.valueOf(70), Integer.valueOf(32));
             put(Integer.valueOf(80), Integer.valueOf(48));
             put(Integer.valueOf(90), Integer.valueOf(64));
-            put(Integer.valueOf(100), Integer.valueOf(4));
-            put(Integer.valueOf(108), Integer.valueOf(5));
-            put(Integer.valueOf(119), Integer.valueOf(6));
-            put(Integer.valueOf(125), Integer.valueOf(8));
-            put(Integer.valueOf(131), Integer.valueOf(12));
-            put(Integer.valueOf(139), Integer.valueOf(32));
-            put(Integer.valueOf(149), Integer.valueOf(48));
-            put(Integer.valueOf(156), Integer.valueOf(80));
-            put(Integer.valueOf(164), Integer.valueOf(112));
-            put(Integer.valueOf(174), Integer.valueOf(128));
-            put(Integer.valueOf(180), Integer.valueOf(144));
-            put(Integer.valueOf(200), Integer.valueOf(16));
-            put(Integer.valueOf(212), Integer.valueOf(48));
-            put(Integer.valueOf(221), Integer.valueOf(80));
-            put(Integer.valueOf(232), Integer.valueOf(112));
-            put(Integer.valueOf(244), Integer.valueOf(144));
-            put(Integer.valueOf(256), Integer.valueOf(176));
-            put(Integer.valueOf(267), Integer.valueOf(192));
-            put(Integer.valueOf(277), Integer.valueOf(208));
-            put(Integer.valueOf(287), Integer.valueOf(224));
-            put(Integer.valueOf(295), Integer.valueOf(240));
-            put(Integer.valueOf(300), Integer.valueOf(5120)); // 20G
+            put(Integer.valueOf(100), Integer.valueOf(80));
+            put(Integer.valueOf(120), Integer.valueOf(96));
+            put(Integer.valueOf(140), Integer.valueOf(112));
+            put(Integer.valueOf(160), Integer.valueOf(128));
+            put(Integer.valueOf(170), Integer.valueOf(144));
+            put(Integer.valueOf(200), Integer.valueOf(4));
+            put(Integer.valueOf(220), Integer.valueOf(32));
+            put(Integer.valueOf(230), Integer.valueOf(64));
+            put(Integer.valueOf(233), Integer.valueOf(96));
+            put(Integer.valueOf(236), Integer.valueOf(128));
+            put(Integer.valueOf(239), Integer.valueOf(160));
+            put(Integer.valueOf(243), Integer.valueOf(192));
+            put(Integer.valueOf(247), Integer.valueOf(224));
+            put(Integer.valueOf(251), Integer.valueOf(256));  //  1G
+            put(Integer.valueOf(300), Integer.valueOf(512));  //  2G
+            put(Integer.valueOf(330), Integer.valueOf(768));  //  3G
+            put(Integer.valueOf(360), Integer.valueOf(1024)); //  4G
+            put(Integer.valueOf(400), Integer.valueOf(1280)); //  5G
+            put(Integer.valueOf(420), Integer.valueOf(1024)); //  4G
+            put(Integer.valueOf(450), Integer.valueOf(768));  //  3G
+            put(Integer.valueOf(500), Integer.valueOf(5120)); // 20G
             
         }};
         this.internalGravityCurve = new RangedCurve(map);
+        
+        map = new TreeMap<Integer, Integer>() {{
+            put(Integer.valueOf(0), Integer.valueOf(30));
+            put(Integer.valueOf(900), Integer.valueOf(17));
+        }};
+        this.lockDelayCurve = new RangedCurve(map);
+
+        map = new TreeMap<Integer, Integer>() {{
+            put(Integer.valueOf(0), Integer.valueOf(25));
+            put(Integer.valueOf(700), Integer.valueOf(16));
+            put(Integer.valueOf(800), Integer.valueOf(12));
+        }};
+        this.areCurve = new RangedCurve(map);
+        
+        map = new TreeMap<Integer, Integer>() {{
+            put(Integer.valueOf(0), Integer.valueOf(40));
+            put(Integer.valueOf(500), Integer.valueOf(25));
+            put(Integer.valueOf(600), Integer.valueOf(16));
+            put(Integer.valueOf(700), Integer.valueOf(12));
+            put(Integer.valueOf(800), Integer.valueOf(6));
+        }};
+        this.lineClearCurve = new RangedCurve(map);
     }
     
     /** {@inheritDoc} */
@@ -67,18 +93,21 @@ public class TheGrandMaster2MasterSpeedCurve implements SpeedCurve {
     /** {@inheritDoc} */
     @Override
     public int computeLockDelay(GameState state) {
-        return 30;
+        int level = state.computeLevel();
+        return lockDelayCurve.getValue(level);
     }
 
     /** {@inheritDoc} */
     @Override
     public int computeARE(GameState state) {
-        return 25;
+        int level = state.computeLevel();
+        return areCurve.getValue(level);
     }
 
     /** {@inheritDoc} */
     @Override
     public int computeLineClearDelay(GameState state) {
-        return 40;
+        int level = state.computeLevel();
+        return lineClearCurve.getValue(level);
     }
 }
