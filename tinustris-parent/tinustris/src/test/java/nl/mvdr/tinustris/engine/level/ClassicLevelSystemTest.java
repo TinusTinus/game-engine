3a271819b57bb9d067d1f623b8683675c08486b3
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-01-01 23:07:42 +0100

    Added a test class for ClassisLevelSystem.

---
+++ tinustris-parent/tinustris/src/test/java/nl/mvdr/tinustris/engine/level/ClassicLevelSystemTest.java
@@ -0,0 +1,154 @@
+package nl.mvdr.tinustris.engine.level;
+
+import nl.mvdr.tinustris.model.GameState;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class for {@link ClassicLevelSystem}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class ClassicLevelSystemTest extends LevelSystemTester {
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test0Lines() {
+        test(0, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test1Lines() {
+        test(1, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test2Lines() {
+        test(2, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test3Lines() {
+        test(3, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test4Lines() {
+        test(4, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test5Lines() {
+        test(5, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test6Lines() {
+        test(6, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test7Lines() {
+        test(7, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test8Lines() {
+        test(8, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test9Lines() {
+        test(9, 0);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test10Lines() {
+        test(10, 1);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test11Lines() {
+        test(11, 1);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test21Lines() {
+        test(21, 2);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test65Lines() {
+        test(65, 6);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test999Lines() {
+        test(999, 99);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test0LinesStartingLevel5() {
+        test(5, 0, 5);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test34LinesStartingLevel5() {
+        test(5, 34, 5);
+    }
+    
+    /** Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void test89LinesStartingLevel5() {
+        test(5, 89, 8);
+    }
+
+    /**
+     * Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}.
+     * 
+     * @param startingLevel starting level
+     * @param lines number of lines
+     * @param expectedLevel expected resulting level
+     */
+    private void test(int lines, int expectedLevel) {
+        test(0, lines, expectedLevel);
+    }
+    
+    /**
+     * Tests {@link ClassicLevelSystem#computeLevel(GameState, GameState)}.
+     * 
+     * @param startingLevel starting level
+     * @param lines number of lines
+     * @param expectedLevel expected resulting level
+     */
+    private void test(int startingLevel, int lines, int expectedLevel) {
+        ClassicLevelSystem levelSystem = new ClassicLevelSystem(startingLevel);
+        GameState state = createGameState(0, lines, 0);
+        
+        int level = levelSystem.computeLevel(state, state);
+        
+        Assert.assertEquals(expectedLevel, level);
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    AbstractLevelSystem createLevelSystem() {
+        return new ClassicLevelSystem();
+    }
+}
---
+++ tinustris-parent/tinustris/src/test/java/nl/mvdr/tinustris/engine/level/LevelSystemTester.java
@@ -0,0 +1,60 @@
+package nl.mvdr.tinustris.engine.level;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import nl.mvdr.tinustris.input.InputStateHistory;
+import nl.mvdr.tinustris.model.GameState;
+import nl.mvdr.tinustris.model.Tetromino;
+
+/**
+ * Abstract superclass for test cases for implementations of {@link LevelSystem}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public abstract class LevelSystemTester {
+    /** Test method that symply invokes {@link AbstractLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void testSameState() {
+        GameState state = createGameState(0, 0, 0);
+        AbstractLevelSystem levelSystem = createLevelSystem();
+        
+        levelSystem.computeLevel(state, state);
+    }
+    
+    /** Test method that symply invokes {@link AbstractLevelSystem#computeLevel(GameState, GameState)}. */
+    @Test
+    public void testDifferentStates() {
+        GameState previousState = createGameState(0, 0, 0);
+        GameState newState = createGameState(0, 0, 0);
+        AbstractLevelSystem levelSystem = createLevelSystem();
+        
+        levelSystem.computeLevel(previousState, newState);
+    }
+    
+    /**
+     * Creates a game state with the given parameters.
+     * 
+     * @param blockCounter block counter
+     * @param level level
+     * @return game state
+     */
+    GameState createGameState(int blockCounter, int lines, int level) {
+        List<Tetromino> grid = new ArrayList<>(220);
+        for (int i = 0; i != 220; i++) {
+            grid.add(null);
+        }
+        GameState state = new GameState(grid, 10, null, null, null, Tetromino.Z, 0, 0, 0, new InputStateHistory(),
+                blockCounter, lines, level);
+        return state;
+    }
+    
+    /**
+     * Creates the level system to be tested.
+     * 
+     * @return level system
+     */
+    abstract AbstractLevelSystem createLevelSystem();
+}
