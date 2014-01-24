438515d038cc0b58144d782aa7707aadc3b1bcf2
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-01-24 09:14:35 +0100

    Added a skeleton for MultiplayerGameEngine.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/engine/MultiplayerEngine.java
@@ -0,0 +1,27 @@
+package nl.mvdr.tinustris.engine;
+
+import java.util.List;
+
+import nl.mvdr.tinustris.input.InputState;
+import nl.mvdr.tinustris.model.MultiplayerGameState;
+
+/**
+ * Implementation of {@link GameEngine} for a multiplayer game.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class MultiplayerEngine implements GameEngine<MultiplayerGameState> {
+    /** {@inheritDoc} */
+    @Override
+    public MultiplayerGameState initGameState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public MultiplayerGameState computeNextState(MultiplayerGameState previousState, List<InputState> inputStates) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
