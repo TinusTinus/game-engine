e48461e9fb90c3d5d3d2d4d0609eb3047fa115c4
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-07 14:23:51 +0100

    Formatting.

--- tinustris/src/main/java/nl/mvdr/tinustris/gui/RectangleBlockCreator.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/gui/RectangleBlockCreator.java
@@ -11,18 +11,18 @@ import nl.mvdr.tinustris.model.Block;
 class RectangleBlockCreator implements BlockCreator {
     /** Size for the arc of a tetromino block. */
     static final int ARC_SIZE = 10;
-    
+
     /** {@inheritDoc} */
     @Override
     public Rectangle createBlock(double xCoordinate, double yCoordinate, double size, Block block, BlockStyle style,
             int numFramesUntilLinesDisappear, int numFramesSinceLastLock) {
         Rectangle result = new Rectangle(xCoordinate, yCoordinate, size, size);
-        
+
         result.setArcWidth(ARC_SIZE);
         result.setArcHeight(ARC_SIZE);
-        
+
         style.apply(result, block, numFramesUntilLinesDisappear, numFramesSinceLastLock);
-        
+
         return result;
     }
 
