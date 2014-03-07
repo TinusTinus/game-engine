a271e748a967dea10e49773c1fcd1474fbe1d0c2
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-07 14:23:39 +0100

    Formatting.

--- tinustris/src/main/java/nl/mvdr/tinustris/gui/BoxBlockCreator.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/gui/BoxBlockCreator.java
@@ -11,7 +11,7 @@ import nl.mvdr.tinustris.model.Block;
 class BoxBlockCreator implements BlockCreator {
     /** Size for the arc of a tetromino block. */
     static final int ARC_SIZE = 10;
-    
+
     /** {@inheritDoc} */
     @Override
     public Box createBlock(double xCoordinate, double yCoordinate, double size, Block block, BlockStyle style,
@@ -19,9 +19,9 @@ class BoxBlockCreator implements BlockCreator {
         Box result = new Box(size - 2, size - 2, size - 2);
         result.setTranslateX(xCoordinate + size / 2);
         result.setTranslateY(yCoordinate + size / 2);
-        
+
         style.apply(result, block, numFramesUntilLinesDisappear, numFramesSinceLastLock);
-        
+
         return result;
     }
 
