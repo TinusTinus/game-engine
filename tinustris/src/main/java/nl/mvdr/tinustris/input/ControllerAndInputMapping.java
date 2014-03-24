2c3bc8f11ca8cd10a9e4fd69e45ecc9b5f329dea
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-24 19:32:04 +0100

    Added a container class.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/input/ControllerAndInputMapping.java
@@ -0,0 +1,24 @@
+package nl.mvdr.tinustris.input;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import net.java.games.input.Controller;
+
+/**
+ * Container class, for a controller and an input mapping.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter(AccessLevel.PACKAGE)
+@ToString
+@EqualsAndHashCode
+class ControllerAndInputMapping {
+    /** Controller. */
+    private final Controller controller;
+    /** Input mapping. */
+    private final InputMapping mapping;
+}
