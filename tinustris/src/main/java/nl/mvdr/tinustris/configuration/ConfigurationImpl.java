40d3354df877cd2974a0a65e5592ead9724f43be
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-25 15:04:01 +0100

    Added getJInputControllerConfiguration(int).

--- tinustris/src/main/java/nl/mvdr/tinustris/configuration/ConfigurationImpl.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/configuration/ConfigurationImpl.java
@@ -1,7 +1,10 @@
 package nl.mvdr.tinustris.configuration;
 
+import java.util.List;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import nl.mvdr.tinustris.gui.GraphicsStyle;
@@ -12,19 +15,31 @@ import nl.mvdr.tinustris.input.JInputControllerConfiguration;
  * 
  * @author Martijn van de Rijdt
  */
-@Getter
 @ToString
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class ConfigurationImpl implements Configuration {
     /** Number of players in the game. Is at least 1. */
+    @Getter
     private final int numberOfPlayers;
     /** Graphical style for the blocks in the grid. */
+    @Getter
+    @NonNull
     private final GraphicsStyle graphicsStyle;
     /** Configuration for the JInutController. */
-    private final JInputControllerConfiguration jInputControllerConfiguration;
+    @NonNull
+    private final List<JInputControllerConfiguration> jInputControllerConfigurations;
     /** Behavior. */
+    @Getter
+    @NonNull
     private final Behavior behavior;
     /** Starting level. */
+    @Getter
     private final int startLevel;
+    
+    /** {@inheritDoc} */
+    @Override
+    public JInputControllerConfiguration getJInputControllerConfiguration(int i) {
+        return this.jInputControllerConfigurations.get(i);
+    }
 }
