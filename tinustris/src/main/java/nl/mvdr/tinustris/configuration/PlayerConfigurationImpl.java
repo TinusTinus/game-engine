ea55736de6731030424a27c2f63471dfc707dd6e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-26 09:45:19 +0100

    Added an implementation of PlayerConfiguration.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/configuration/PlayerConfigurationImpl.java
@@ -0,0 +1,26 @@
+package nl.mvdr.tinustris.configuration;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import nl.mvdr.tinustris.input.JInputControllerConfiguration;
+
+/**
+ * Implementation of {@link PlayerConfiguration}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class PlayerConfigurationImpl implements PlayerConfiguration {
+    /** Player name. */
+    @NonNull
+    private final String name;
+    /** Configuration for the JInputController for this player. */
+    @NonNull
+    private final JInputControllerConfiguration jInputControllerConfiguration;
+}
