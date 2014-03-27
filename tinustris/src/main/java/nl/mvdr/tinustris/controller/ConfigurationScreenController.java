7feae082d16930638c231e471636f93b6d56f564
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-27 18:20:46 +0100

    Disabled property is now set correctly for the graphics radio buttons.

--- tinustris/src/main/java/nl/mvdr/tinustris/controller/ConfigurationScreenController.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/ConfigurationScreenController.java
@@ -2,6 +2,7 @@ package nl.mvdr.tinustris.controller;
 
 import java.util.stream.Stream;
 
+import nl.mvdr.tinustris.gui.GraphicsStyle;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import lombok.extern.slf4j.Slf4j;
@@ -28,9 +29,29 @@ public class ConfigurationScreenController {
         Stream.of(graphics2DRadioButton, graphics3DRadioButton)
             .forEach(radioButton ->
                 radioButton.setOnAction(event -> log.info("Activated " + radioButton.getText())));
+        Stream.of(graphics2DRadioButton, graphics3DRadioButton)
+            .forEach(radioButton -> radioButton.setDisable(!toGraphicsStyleValue(radioButton).isAvailable()));
         
         // TODO further initialisation
         
         log.info("Initialisation complete.");
+    }
+
+    /**
+     * Returns the graphics style value corresponding to the given ui component.
+     * 
+     * @param radioButton ui component; must be one of the radio buttons corresponding to a graphical style
+     * @return graphics style
+     */
+    private GraphicsStyle toGraphicsStyleValue(RadioButton radioButton) {
+        GraphicsStyle result;
+        if (radioButton == graphics2DRadioButton) {
+            result = GraphicsStyle.TWO_DIMENSIONAL;
+        } else if (radioButton == graphics3DRadioButton) {
+            result = GraphicsStyle.THREE_DIMENSIONAL;
+        } else {
+            throw new IllegalArgumentException("Unexpected parameter: " + radioButton);
+        }
+        return result;
     }
 }
