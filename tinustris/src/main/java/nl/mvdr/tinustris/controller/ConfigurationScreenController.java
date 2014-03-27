fcbcd38d65dbe05a27beff26ed20f9d589ef7c8c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-27 08:50:52 +0100

    Added a controller.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/ConfigurationScreenController.java
@@ -0,0 +1,20 @@
+package nl.mvdr.tinustris.controller;
+
+import javafx.fxml.FXML;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Controller for a configuration screen.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Slf4j
+public class ConfigurationScreenController {
+    /** Initialisation method. */
+    @FXML
+    private void initialize() {
+        log.info("Initialising.");
+        
+        // TODO initialisation
+    }
+}
