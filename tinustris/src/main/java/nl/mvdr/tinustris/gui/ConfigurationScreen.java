988501b254f45154e5c11436927af48407136307
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-27 08:42:18 +0100

    Added configuration screen view class.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/gui/ConfigurationScreen.java
@@ -0,0 +1,35 @@
+package nl.mvdr.tinustris.gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Main class which opens the configuration screen.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class ConfigurationScreen extends Application {
+
+    /** {@inheritDoc} */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/Configuration.fxml"));
+        
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are passed on to JavaFX
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
