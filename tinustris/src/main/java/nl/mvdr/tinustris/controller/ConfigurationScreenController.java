7191a56d2d2bfe999c9630879202bf99c20e5e6b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-30 20:58:53 +0200

    Using PlayerConfigurationImpl rather than the interface.

--- tinustris/src/main/java/nl/mvdr/tinustris/controller/ConfigurationScreenController.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/ConfigurationScreenController.java
@@ -23,7 +23,9 @@ import nl.mvdr.tinustris.configuration.Behavior;
 import nl.mvdr.tinustris.configuration.Configuration;
 import nl.mvdr.tinustris.configuration.ConfigurationImpl;
 import nl.mvdr.tinustris.configuration.PlayerConfiguration;
+import nl.mvdr.tinustris.configuration.PlayerConfigurationImpl;
 import nl.mvdr.tinustris.gui.GraphicsStyle;
+import nl.mvdr.tinustris.input.JInputControllerConfiguration;
 
 /**
  * Controller for a configuration screen.
@@ -215,12 +217,12 @@ public class ConfigurationScreenController {
      * @return Configuration
      */
     private Configuration buildConfiguration() {
-        // TODO retrieve input configuration from the actual playerTabPane and construct a PlayerConfigurationImpl
+        // TODO retrieve input configuration from the actual playerTabPane instead of using the default
         List<PlayerConfiguration> playerConfigurations = playerTabPane.getTabs()
                 .subList(0, playerTabPane.getTabs().size() - 1)
                 .stream()
                 .map(Tab::getText)
-                .map(name -> (PlayerConfiguration) () -> name)
+                .map(name -> new PlayerConfigurationImpl(name, JInputControllerConfiguration.defaultConfiguration()))
                 .collect(Collectors.toList());
         
         GraphicsStyle graphicsStyle = toGraphicsStyle(graphics2DRadioButton.getToggleGroup().getSelectedToggle());
