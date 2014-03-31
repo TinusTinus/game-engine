3e54e11b4437b0f80f551ee4bf52af9542925780
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-31 21:44:21 +0200

    Nice toString.

--- tinustris/src/main/java/nl/mvdr/tinustris/input/Input.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/input/Input.java
@@ -14,20 +14,28 @@ import nl.mvdr.tinustris.model.Action;
 @Getter
 public enum Input {
     /** Moves the currently active block left. */
-    LEFT(Action.MOVE_LEFT),
+    LEFT(Action.MOVE_LEFT, "Move left"),
     /** Moves the currently active block right. */
-    RIGHT(Action.MOVE_RIGHT),
+    RIGHT(Action.MOVE_RIGHT, "Move right"),
     /** Accelerates the currently active block's descent. */
-    SOFT_DROP(Action.MOVE_DOWN),
+    SOFT_DROP(Action.MOVE_DOWN, "Soft drop"),
     /** Instantly drops the currently active block. */
-    HARD_DROP(Action.HARD_DROP),
+    HARD_DROP(Action.HARD_DROP, "Hard drop"),
     /** Rotates the currently active block 90 degrees clockwise. */
-    TURN_RIGHT(Action.TURN_RIGHT),
+    TURN_RIGHT(Action.TURN_RIGHT, "Turn right"),
     /** Rotates the currently active block 90 degrees counter-clockwise. */
-    TURN_LEFT(Action.TURN_LEFT),
+    TURN_LEFT(Action.TURN_LEFT, "Turn left"),
     /** Swaps the currently active block and the next block. */
-    HOLD(Action.HOLD);
+    HOLD(Action.HOLD, "Hold");
     
     /** Action to be taken when this input is pressed. */
     private final Action action;
+    /** Nice name. */
+    private final String name;
+    
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return name;
+    }
 }
