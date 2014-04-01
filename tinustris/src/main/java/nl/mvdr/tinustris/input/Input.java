2b11622318cb6b5603225ca88afb94962a95498e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-04-01 14:18:46 +0200

    Added a description for each input.

--- tinustris/src/main/java/nl/mvdr/tinustris/input/Input.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/input/Input.java
@@ -14,24 +14,26 @@ import nl.mvdr.tinustris.model.Action;
 @Getter
 public enum Input {
     /** Moves the currently active block left. */
-    LEFT(Action.MOVE_LEFT, "Move left"),
+    LEFT(Action.MOVE_LEFT, "Move left", "Shifts the tetromino one column to the left."),
     /** Moves the currently active block right. */
-    RIGHT(Action.MOVE_RIGHT, "Move right"),
+    RIGHT(Action.MOVE_RIGHT, "Move right", "Shifts the tetromino one column to the right."),
     /** Accelerates the currently active block's descent. */
-    SOFT_DROP(Action.MOVE_DOWN, "Soft drop"),
+    SOFT_DROP(Action.MOVE_DOWN, "Soft drop", "Accelerates the tetromino's descent."),
     /** Instantly drops the currently active block. */
-    HARD_DROP(Action.HARD_DROP, "Hard drop"),
+    HARD_DROP(Action.HARD_DROP, "Hard drop", "Instantly drops the tetromino down."),
     /** Rotates the currently active block 90 degrees clockwise. */
-    TURN_RIGHT(Action.TURN_RIGHT, "Turn right"),
+    TURN_RIGHT(Action.TURN_RIGHT, "Turn right", "Rotates the tetromino 90 degrees clockwise."),
     /** Rotates the currently active block 90 degrees counter-clockwise. */
-    TURN_LEFT(Action.TURN_LEFT, "Turn left"),
+    TURN_LEFT(Action.TURN_LEFT, "Turn left", "Rotates the tetromino 90 degrees counter-clockwise."),
     /** Swaps the currently active block and the next block. */
-    HOLD(Action.HOLD, "Hold");
+    HOLD(Action.HOLD, "Hold", "Swaps the current tetromino with the next one.");
     
     /** Action to be taken when this input is pressed. */
     private final Action action;
     /** Nice name. */
     private final String name;
+    /** Description of the effect of this input. */
+    private final String description;
     
     /** {@inheritDoc} */
     @Override
