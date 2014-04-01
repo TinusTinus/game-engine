10d87457bfa8ad5906eb86ec58ff0b0826633b0f
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-04-01 14:38:19 +0200

    Added fields and initialisation method.

--- tinustris/src/main/java/nl/mvdr/tinustris/controller/InputConfigurationController.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/InputConfigurationController.java
@@ -1,7 +1,9 @@
 package nl.mvdr.tinustris.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import lombok.extern.slf4j.Slf4j;
+import nl.mvdr.tinustris.input.Input;
 
 /**
  * Controller for ithe input configuration screen.
@@ -10,6 +12,43 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class InputConfigurationController {
+    /** Button prompt label. */
+    @FXML
+    private Label buttonPromptLabel;
+    /** Description label. */
+    @FXML
+    private Label descriptionLabel;
+    
+    /** Initialisation method. */
+    // default visibility for unit test
+    @FXML
+    void initialize() {
+        log.info("Initialising controller for input configuration.");
+        if (log.isDebugEnabled()) {
+            log.debug(this.toString());
+        }
+
+        // TODO more initialisation
+        
+        Input input = Input.LEFT;
+        updateLabels(input);
+        
+        log.info("Initialisation complete.");
+        if (log.isDebugEnabled()) {
+            log.debug(this.toString());
+        }
+    }
+
+    /**
+     * Updates the label text for the given input.
+     * 
+     * @param input input
+     */
+    private void updateLabels(Input input) {
+        buttonPromptLabel.setText(String.format("Please press the button for %s.", input));
+        descriptionLabel.setText(input.getDescription());
+    }
+    
     /** Handler for the cancel button. */
     @FXML
     private void cancel() {
