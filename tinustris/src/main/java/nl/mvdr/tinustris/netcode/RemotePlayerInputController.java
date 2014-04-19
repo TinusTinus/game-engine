35c5aaf24d6fb611cc2bf7678ef90423492e90ca
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-04-19 22:08:49 +0200

    Added the start of an input controller for remote players.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/netcode/RemotePlayerInputController.java
@@ -0,0 +1,41 @@
+package nl.mvdr.tinustris.netcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import nl.mvdr.tinustris.input.InputController;
+import nl.mvdr.tinustris.input.InputState;
+
+/**
+ * Input controller for a remote player.
+ * 
+ * Note that this class is stateful.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class RemotePlayerInputController implements InputController {
+    /** Map of frame index to the received input states. */
+    private final Map<Integer, InputState> states;
+    
+    /** Constructor. */
+    public RemotePlayerInputController() {
+        super();
+        this.states = new HashMap<>();
+    }
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * Returns the latest known input state, or a default input state if none are known. This may (and probably will) be
+     * an out of date one.
+     */
+    @Override
+    public InputState getInputState() {
+        return states.entrySet()
+            .stream()
+            .max((left, right) -> Integer.compare(left.getKey(), right.getKey()))
+            .map(Entry<Integer, InputState>::getValue)
+            .orElse(input -> false);
+    }
+}
