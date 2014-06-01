3464f719514d219ccf573c0ddec6093e0e9e6c82
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-06-01 14:33:11 +0200

    Removed TODO.

--- tinustris/src/main/java/nl/mvdr/tinustris/engine/GameLoop.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/engine/GameLoop.java
@@ -95,7 +95,6 @@ public class GameLoop<S extends GameState> {
         log.info("Starting main game loop.");
 
         try {
-            // TODO the following guard may not be correct in a netcode game!
             while (running && !holder.isGameOver()) {
                 double now = System.nanoTime();
                 int updateCount = 0;
