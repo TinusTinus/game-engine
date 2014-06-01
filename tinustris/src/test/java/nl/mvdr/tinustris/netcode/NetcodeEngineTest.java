0f041df62c693bc9c9a63d7cc96b92aca1804098
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-06-01 14:47:24 +0200

    Added test cases.

--- tinustris/src/test/java/nl/mvdr/tinustris/netcode/NetcodeEngineTest.java
+++ tinustris/src/test/java/nl/mvdr/tinustris/netcode/NetcodeEngineTest.java
@@ -15,6 +15,7 @@ import nl.mvdr.tinustris.model.GameState;
 import nl.mvdr.tinustris.netcode.NetcodeEngine;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -90,5 +91,78 @@ public class NetcodeEngineTest {
         gameStateHolder.accept(new FrameAndInputStatesContainer(0, inputStates));
         
         Assert.assertSame(state, inputHolder.getInputState());
+    }
+    
+    
+    /** Tests the {@link NetcodeEngine#isGameOver()} method, in case the latest game state is not a game over. */
+    @Test
+    public void testGameNotOver() {
+        NetcodeEngine<DummyGameState> holder = new NetcodeEngine<>(Collections.<InputStateHolder> emptyList(),
+                new DummyGameEngine());
+        holder.addGameState(DummyGameState.GAME_NOT_OVER);
+        
+        Assert.assertFalse(holder.isGameOver());
+    }
+    
+    /** Tests the {@link NetcodeEngine#isGameOver()} method, in case there is just one game state, which is game over. */
+    @Test
+    public void testGameOver() {
+        NetcodeEngine<DummyGameState> holder = new NetcodeEngine<>(Collections.<InputStateHolder> emptyList(),
+                new DummyGameEngine());
+        holder.addGameState(DummyGameState.GAME_OVER);
+        
+        Assert.assertTrue(holder.isGameOver());        
+    }
+
+    /**
+     * Tests the {@link NetcodeEngine#isGameOver()} method, in case there have been multiple game state updates, all
+     * input states are known, and the game is over.
+     */
+    @SuppressWarnings("serial") // local test maps, not to be serialised
+    @Test
+    public void testGameOverAfterSeveralStates() {
+        NetcodeEngine<DummyGameState> holder = new NetcodeEngine<>(
+                Arrays.asList(new InputStateHolder(false), new InputStateHolder(false)),
+                new DummyGameEngine());
+        holder.addGameState(DummyGameState.GAME_NOT_OVER);
+        holder.accept(new FrameAndInputStatesContainer(0, new HashMap<Integer, InputState>() {{
+            put(0, input -> false);
+            put(1, input -> false);
+        }}));
+        holder.addGameState(DummyGameState.GAME_NOT_OVER);
+        holder.accept(new FrameAndInputStatesContainer(1, new HashMap<Integer, InputState>() {{
+            put(0, input -> false);
+            put(1, input -> false);
+        }}));
+        holder.addGameState(DummyGameState.GAME_OVER);
+        
+        Assert.assertTrue(holder.isGameOver());        
+    }
+    
+    /**
+     * Tests the {@link NetcodeEngine#isGameOver()} method, in case there have been multiple game state updates, but not
+     * all input states are known.
+     */
+    @SuppressWarnings("serial") // local test maps, not to be serialised
+    @Test
+    @Ignore // TODO implement this functionality
+    public void testGameOverAfterSeveralStatesIncompleteInputs() {
+        NetcodeEngine<DummyGameState> holder = new NetcodeEngine<>(
+                Arrays.asList(new InputStateHolder(false), new InputStateHolder(true)),
+                new DummyGameEngine());
+        holder.addGameState(DummyGameState.GAME_NOT_OVER);
+        holder.accept(new FrameAndInputStatesContainer(0, new HashMap<Integer, InputState>() {{
+            put(0, input -> false);
+        }}));
+        holder.accept(new FrameAndInputStatesContainer(0, new HashMap<Integer, InputState>() {{
+            put(1, input -> false);
+        }}));
+        holder.addGameState(DummyGameState.GAME_NOT_OVER);
+        holder.accept(new FrameAndInputStatesContainer(1, new HashMap<Integer, InputState>() {{
+            put(0, input -> false);
+        }}));
+        holder.addGameState(DummyGameState.GAME_OVER);
+        
+        Assert.assertFalse(holder.isGameOver());        
     }
 }
