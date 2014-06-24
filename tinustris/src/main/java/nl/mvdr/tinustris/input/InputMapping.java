ad4278a1c9f0b9ed7f330728fd2bbdf502b3c404
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-06-24 16:50:28 +0200

    Extracted a method.

--- tinustris/src/main/java/nl/mvdr/tinustris/input/InputMapping.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/input/InputMapping.java
@@ -34,30 +34,36 @@ public class InputMapping {
         
         if (component.getIdentifier() == Axis.POV) {
             // d-pad
-            if (pressedValue == POV.UP) {
-                result = "D-pad up";
-            } else if (pressedValue == POV.UP_RIGHT) {
-                result = "D-pad up-right";
-            } else if (pressedValue == POV.RIGHT) {
-                result = "D-pad right";
-            } else if (pressedValue == POV.DOWN_RIGHT) {
-                result = "D-pad down-right";
-            } else if (pressedValue == POV.DOWN) {
-                result = "D-pad down";
-            } else if (pressedValue == POV.DOWN_LEFT) {
-                result = "D-pad down-left";
-            } else if (pressedValue == POV.LEFT) {
-                result = "D-pad left";
-            } else if (pressedValue == POV.UP_LEFT) {
-                result = "D-pad up-left";
-            } else {
-                // unknown value
-                result = "D-pad";
-            }
+            result = toStringDPad();
         } else {
             result = component.getName();
         }
         return result;
     }
-    
+
+    /** @return toString value in case this mapping corresponds to a d-pad component */
+    private String toStringDPad() {
+        String result;
+        if (pressedValue == POV.UP) {
+            result = "D-pad up";
+        } else if (pressedValue == POV.UP_RIGHT) {
+            result = "D-pad up-right";
+        } else if (pressedValue == POV.RIGHT) {
+            result = "D-pad right";
+        } else if (pressedValue == POV.DOWN_RIGHT) {
+            result = "D-pad down-right";
+        } else if (pressedValue == POV.DOWN) {
+            result = "D-pad down";
+        } else if (pressedValue == POV.DOWN_LEFT) {
+            result = "D-pad down-left";
+        } else if (pressedValue == POV.LEFT) {
+            result = "D-pad left";
+        } else if (pressedValue == POV.UP_LEFT) {
+            result = "D-pad up-left";
+        } else {
+            // unknown value
+            result = "D-pad";
+        }
+        return result;
+    }
 }
