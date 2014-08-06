64f5b2d052a82bd0cc9c9131fe8c51c649bbaed1
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-08-06 18:00:05 +0200

    Added a controller.

---
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/NetplayConfigurationScreenController.java
@@ -0,0 +1,19 @@
+package nl.mvdr.tinustris.controller;
+
+import javafx.fxml.FXML;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Controller for the netplay configuration screen.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Slf4j
+public class NetplayConfigurationScreenController {
+    /** Handles the player activating the "next" button. */
+    @FXML
+    private void next() {
+        log.info("Next button activated.");
+        // TODO
+    }
+}
