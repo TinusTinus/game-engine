903c3dc9858e622896bbbe27d55e2a9a56559064
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-08-06 17:24:02 +0200

    Added a launcher for the test classpath.

---
+++ tinustris/src/test/java/nl/mvdr/tinustris/gui/NetplayConfigurationScreenTestContext.java
@@ -0,0 +1,31 @@
+package nl.mvdr.tinustris.gui;
+
+/**
+ * Main class, whose main method simply calls {@link NetplayConfigurationScreen#main(String[])}.
+ * 
+ * Workaround for the fact that the test classpath (which contains useful things such as a log4j configuration) does not
+ * seem to be available when running {@link NetplayConfigurationScreen#main(String[])} directly in Eclipse Kepler.
+ * 
+ * This class relies on JInput's native libraries. These are not available by default. If you want to run this
+ * class, make sure that the java.library.path system property contains target/natives in this project directory.
+ * 
+ * In Eclipse, you can do this by opening the Run Configuration, opening the arguments tab and pasting the following
+ * into the "VM Arguments" text area:
+ * 
+ * <pre>
+ * -Djava.library.path=${project_loc}/target/natives
+ * </pre>
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class NetplayConfigurationScreenTestContext {
+    /**
+     * Calls {@link ConfigurationScreen#main(String[])}.
+     * 
+     * @param args
+     *            command line parameters
+     */
+    public static void main(String[] args) {
+        NetplayConfigurationScreen.main(args);
+    }
+}
