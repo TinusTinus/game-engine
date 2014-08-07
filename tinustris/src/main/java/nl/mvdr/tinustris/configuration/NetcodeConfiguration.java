9f04c5df0a30ab3aa697df727c3823ce077ae00b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-08-07 18:42:21 +0200

    Removed unnecessary public modifier.

--- tinustris/src/main/java/nl/mvdr/tinustris/configuration/NetcodeConfiguration.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/configuration/NetcodeConfiguration.java
@@ -12,7 +12,7 @@ public interface NetcodeConfiguration {
     /** Port used for netplay. */
     // TODO make this a configuration option?
     // TODO choose a more appropriate port? see http://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
-    public static final int PORT = 8082;
+    static final int PORT = 8082;
     
     /** @return configuration for each remote game instance */
     List<RemoteConfiguration> getRemotes();
