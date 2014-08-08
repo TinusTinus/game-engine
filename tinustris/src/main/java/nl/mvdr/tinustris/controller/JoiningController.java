cd1766722bbe734f0ca34100cfa5c62565d41e87
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-08-08 17:11:41 +0200

    Implemented cancel.

--- tinustris/src/main/java/nl/mvdr/tinustris/controller/JoiningController.java
+++ tinustris/src/main/java/nl/mvdr/tinustris/controller/JoiningController.java
@@ -1,7 +1,11 @@
 package nl.mvdr.tinustris.controller;
 
+import java.io.IOException;
+
+import nl.mvdr.tinustris.gui.NetplayConfigurationScreen;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,11 +27,32 @@ public class JoiningController {
         log.info("Controller initialised: {}", this);
     }
     
-    /** Handler for the cancel button. */
+    /**
+     * Handler for the cancel button.
+     * 
+     * @throws IOException
+     *             In case the netplay configuration screen's fxml cannot be loaded. This should not occur. In order to
+     *             get to the Hosting screen the Netplay Configuration screen must have already been loaded succesfully
+     *             once. Let the thread's exception handler log the error and let the user close the window manually.
+     */
     @FXML
-    private void cancel() {
+    private void cancel() throws IOException {
         log.info("Cancelling.");
-        // TODO implement
+        returnToNetplayConfigurationScreen();
+    }
+    
+    /**
+     * Returns the user to the netplay configuration screen.
+     * 
+     * @throws IOException
+     *             In case the netplay configuration screen's fxml cannot be loaded. This should not occur. In order to
+     *             get to the Hosting screen the Netplay Configuration screen must have already been loaded succesfully
+     *             once. Let the thread's exception handler log the error and let the user close the window manually.
+     */
+    private void returnToNetplayConfigurationScreen() throws IOException {
+        Stage stage = retrieveStage();
+        NetplayConfigurationScreen firstScreen = new NetplayConfigurationScreen();
+        firstScreen.start(stage);
     }
     
     /** Handler for the join button. */
@@ -35,5 +60,10 @@ public class JoiningController {
     private void join() {
         log.info("Joining game.");
         // TODO implement
+    }
+    
+    /** Gets the stage associated to this controller. */
+    private Stage retrieveStage() {
+        return (Stage) hostTextField.getScene().getWindow();
     }
 }
